3f942c1ce38175f21521bc7f3bf5e1c6ee59c626
Drioty <43117815+Drioty@users.noreply.github.com>
2018-09-20 10:29:25 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
 a = int(input())
 b = int(input())
 count = 0
+k = 0
 while (a > 0) and (b > 0):
     if a % 2 != b % 2:
         count += 1
@@ -8,10 +9,12 @@ while (a > 0) and (b > 0):
     b = b // 2
 if a > 0:
     while a > 0:
-        count += 1
+        if a % 2 == 1:
+            k += 1
         a = a // 2
 if b > 0:
     while b > 0:
-        count += 1
+        if b % 2 == 1:
+            k += 1
         b = b // 2
-print(count)
+print(count + k)
